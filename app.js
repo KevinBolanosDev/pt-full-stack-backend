6b9c4587e4dcdb0fee3a4670787b4373fe21bff0
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2025-04-02 11:51:20 -0500

    Modificando variables para deploy

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 // Configuración de CORS
 app.use(
     cors({
-        origin: process.env.FRONTED_URL,
+        origin: process.env.FRONTEND_URL,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     })
